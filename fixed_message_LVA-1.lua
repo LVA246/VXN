08c0481d314c4bfd156d4374d8f69931e07695ee
LVA246 <thabronx3plug15@gmail.com>
2025-11-29 18:57:03 -0500

    Add files via upload

---
+++ fixed_message_LVA-1.lua
@@ -0,0 +1,481 @@
+local Config = {
+    ["TheLVA"] = {
+        ["Selected_Item"] = "...";
+
+        ["TeleportationList"] = {
+            ["Deli Market 🥪"] = CFrame.new(-602.3944091796875, 253.73313903808594, -584.2000732421875);
+            ["Capital One Bank 🏦"] = CFrame.new(-223, 284, -1200);
+            ["Ice Box 🧊"] = CFrame.new(-198.8927001953125, 283.8486633300781, -1170.4500732421875);
+            ["Domino's 🍕"] = CFrame.new(-742.5213012695312, 253.22897338867188, -946.2092895507812);
+            ["Hotel 🏨"] = CFrame.new(-1012, 266, -933);
+            ["Drip Store 👓"] = CFrame.new(67462.6953125, 10489.0322265625, 546.6762084960938);
+            ["Gun Shop 🔫"] = CFrame.new(92970.4140625, 122097.953125, 17023.623046875);
+            ["Car Dealer 🚗"] = CFrame.new(-378.6668701171875, 253.2564697265625, -1245.4259033203125);
+            ["Laundromat 💷"] = CFrame.new(-979.4635620117188, 253.65318298339844, -689.3339233398438);
+            ["Studio 🎙"] = CFrame.new(93408.453125, 14484.7158203125, 570.139404296875);
+            ["Basketball Court 🏀"] = CFrame.new(-1055.6407470703125, 253.51364135742188, -497.10528564453125);
+            ["Robbable Ice Box 🧊"] = CFrame.new(-209.68360900878906, 283.4959411621094, -1265.5286865234375);
+            ["Exotic Dealer / Grass House 🍃"] = CFrame.new(-1521.943115234375, 272.5462646484375, -984.3020629882812);
+            ["Safe 🔒"] = CFrame.new(-190, 295, -1010);
+            ["Roof Top / Bank Tools 🛠"] = CFrame.new(-385, 340, -557);
+            ["Second Gun Shop 🔫"] = CFrame.new(66202, 123615.7109375, 5749.81689453125);
+            ["Construction Job 🔨"] = CFrame.new(-1729, 371, -1171);
+            ["Hospital🚑"] = CFrame.new(-1590, 254, 17);
+        };
+
+        ["Modifications"] = newproxy(true);
+
+        ["_Modifications"] = {
+            ["DisableJamming"] = false;
+            ["ModifySpreadValue"] = false;
+            ["ModifyRecoilValue"] = false;
+            ["Automatic"] = false;
+            ["ModifyFireRate"] = false;
+            ["ModifyReloadSpeed"] = false;
+            ["ModifyEquipSpeed"] = false;
+            ["InfiniteAmmo"] = false;
+            ["InfiniteClips"] = false;
+            ["InfiniteDamage"] = false;
+
+            ["FireRateSpeed"] = 50;
+            ["SpreadPercentage"] = 50;
+            ["RecoilPercentage"] = 50;
+            ["ReloadSpeed"] = 50;
+            ["EquipSpeed"] = 50;
+        };
+
+        ["PlayerModifications"] = {
+            ["InfiniteSleep"] = false;
+            ["InfiniteStamina"] = false;
+            ["InfiniteHunger"] = false;
+            ["InstantInteract"] = false;
+            ["InstantRevive"] = false;
+            ["AutoPickupCash"] = false;
+            ["AutoPickupBags"] = false;
+            ["DisableCameraBobbing"] = false;
+            ["DisableCameras"] = false;
+            ["BypassLockedCars"] = false;
+            ["DisableBloodEffects"] = false;
+            ["NoJumpCooldown"] = false;
+            ["NoRentPay"] = false;
+            ["NoFallDamage"] = false;
+            ["NoKnockback"] = false;
+            ["InfiniteHealth"] = false;
+            ["RespawnWhereYouDied"] = false;
+        };
+
+        ["InfiniteHealth"] = false;
+
+        ["StoreDupedItem"] = false;
+        ["Selected_Location"] = "...";
+        ["ClickTeleportActive"] = false;
+
+        ["PlayerUtilities"] = {
+            ["SelectedPlayer"] = "...";
+            ["BringingPlayer"] = false;
+            ["SpectatePlayer"] = false;
+            ["AutoKill"] = false;
+            ["AutoRagdoll"] = false;
+            ["BugPlayer"] = false;
+        };
+
+        ["VehicleModifications"] = {
+            ["SpeedEnabled"] = false;
+            ["SpeedValue"] = 10/1000;
+            ["BreakEnabled"] = false;
+            ["BreakValue"] = 50/1000;
+            ["InstantStop"] = false;
+            ["InstantStopBind"] = Enum.KeyCode.V;
+        };
+
+        ["Farms"] = {
+            ["CollectDroppedMoney"] = false;
+
+            ["CollectDroppedLoot"] = false;
+            ["OnlyCollectGuns"] = false;
+
+            ["AFKCheck"] = false;
+            ["FarmConstructionJob"] = false;
+            ["FarmBank"] = false;
+            ["FarmHouses"] = false;
+            ["FarmStudio"] = false;
+            ["FarmTrash"] = false;
+            ["AutoSellTrash"] = false;
+        };
+
+        ["KillAura"] = false;
+        ["KillAuraRange"] = 300;
+        ["KillAuraWhitelist"] = {};
+
+        ["AutoDrop"] = false;
+        ["MoneyAmount"] = false;
+
+        ["Fly"] = {
+            ["Enabled"] = false;
+            ["Type"] = "CFrame";
+            ["Speed"] = 50;
+        };
+    };
+
+    ["BlockSpin"] = {
+        ["LocalPlayer"] = {
+            ["InfiniteStamina"] = false;
+        };
+
+        ["AutoFarming"] = {
+            ["FarmMops"] = false;
+            ["MopType"] = "Default";
+        };
+    };
+
+    ["Road_To_Riches"] = {
+        ["_Modifications"] = {
+            ["ExplosiveBullets"] = false;
+            ["InfiniteAmmo"] = false;
+            ["InfiniteDamage"] = false;
+            ["InstantReload"] = false;
+            ["InstantEquip"] = false;
+            ["Automatic"] = false;
+            ["RapidFire"] = false;
+            ["NoSpread"] = false;
+            ["NoRecoil"] = false;
+        };
+
+        ["Locations"] = {
+            ["ATM 🏧"] = CFrame.new(360, 2563, 1946);
+            ["Gun Dealer 🔫"] = CFrame.new(231, 2545, 1065);
+            ["Gold Shop 🪙"] = CFrame.new(-78.1158218383789, 2563.1123046875, 944.74365234375);
+            ["Barber 💈"] = CFrame.new(1217.6531982421875, 2557.93896484375, 981.182861328125);
+            ["Monderella 🍕"] = CFrame.new(428.89581298828125, 2563.31005859375, 1275.4918212890625);
+            ["Deli 🥪"] = CFrame.new(-70.2744369506836, 2562.807861328125, 719.9676513671875);
+            ["Library 📖"] = CFrame.new(53.477169036865234, 2562.908203125, 1960.6444091796875);
+            ["Laboratory 🔬"] = CFrame.new(564, 2563, 1957);
+            ["Hot Dog Stand 🌭"] = CFrame.new(295.57403564453125, 2562.7646484375, 1996.2435302734375);
+            ["Gym 💪"] = CFrame.new(-37.920719146728516, 2563.05029296875, 442.32647705078125);
+            ["Banks Burgers 🍔"] = CFrame.new(794.2547607421875, 2562.93701171875, 311.8097229003906);
+            ["Laundromat 💷"] = CFrame.new(402.6617126464844, 2562.892822265625, 1503.2645263671875);
+            ["Cocaine Factory 🏭"] = CFrame.new(1227, 2529, 1987);
+            ["Cooking Pots 🧪"] = CFrame.new(131, 2540, 1004);
+            ["Scrap Metal ⚒️"] = CFrame.new(1049, 2563, 1052);
+        };
+
+        ["PackFarm"] = {
+            ["Enabled"] = false;
+            ["AllowedPacks"] = {};
+            ["AllowedJunkies"] = {};
+        };
+
+        ["InfiniteClips"] = false;
+        ["InfiniteStamina"] = false;
+        ["InfiniteEnergy"] = false;
+
+        ["Modifications"] = newproxy(true);
+
+        ["InstantInteract"] = false;
+
+        ["ESP"] = {
+            ["Pots"] = false;
+            ["AllowedPots"] = {};
+        };
+    };
+
+    ["South_LVA"] = {
+        ["LocalPlayer_Config"] = {
+            ["InstantInteract"] = false;
+            ["DeleteOnKey"] = false;
+            ["DeleteKey"] = nil;
+            ["NoClip"] = false;
+            ["HideName"] = false;
+            ["InfiniteStamina"] = false;
+            ["Speed"] = false;
+            ["SpeedValue"] = 0.75;
+        };
+
+        ["TeleportMethod"] = "Damage";
+
+        ["FarmingUtilities"] = {
+            ["CardFarm"] = false;
+            ["BoxFarm"] = false;
+            ["ChipFarm"] = false;
+            ["MarshmallowFarm"] = false;
+            ["MarshmallowIncrement"] = 5;
+        };
+
+        ["OwnedBike"] = "Unknown";
+
+        ["Guns"] = {};
+
+        ["Selected_Item"] = "...";
+        ["Item_Amount"] = 1;
+
+        ["Locations"] = {
+            ["Main Gun Store 🔫"] = CFrame.new(219, 6, -158);
+            ["Black Market 💹"] = CFrame.new(671, 6, 251);
+            ["DealerShip 🚗"] = CFrame.new(738, 6, 439);
+            ["DealerShip Apartments 🌇"] = CFrame.new(717, 5, 548);
+            ["Clothes Store 👕"] = CFrame.new(-197, 6, -74);
+            ["Box Job Apartments 📦"] = CFrame.new(-527, 6, 142);
+            ["Bank 💳"] = CFrame.new(-47, 6, -340);
+            ["Fake ID Seller 🎫"] = CFrame.new(219, 6, -331);
+            ["DOA Turf 🔴"] = CFrame.new(-335, 6, -415);
+            ["OGZ Turf 🟣"] = CFrame.new(125, 6, -466);
+            ["YGZ Turf 🟢"] = CFrame.new(3, 6, 223);
+            ["Studio 🎙"] = CFrame.new(522, 6, -26);
+            ["Shoe Store 👟"] = CFrame.new(525, 7, -184);
+            ["Second Gun Store 🔫"] = CFrame.new(-459, 6, 328);
+            ["Exclusive Gun Store 🔫"] = CFrame.new(1131, 4, 173);
+        };
+
+        ["Selected_Location"] = "...";
+
+        ["Modifications"] = newproxy(true);
+
+        ["_Modifications"] = {
+            ["DisableJamming"] = false;
+            ["ModifySpreadValue"] = false;
+            ["ModifyRecoilValue"] = false;
+            ["Automatic"] = false;
+            ["ModifyFireRate"] = false;
+            ["InstantKill"] = false;
+            ["ModifyReloadSpeed"] = false;
+            ["ModifyEquipSpeed"] = false;
+            ["InfiniteAmmo"] = false;
+            ["InfiniteClips"] = false;
+
+            ["FireRateSpeed"] = 50;
+            ["SpreadPercentage"] = 50;
+            ["RecoilPercentage"] = 50;
+            ["ReloadSpeed"] = 50;
+            ["EquipSpeed"] = 50;
+        };
+
+        ["PlayerUtilities"] = {
+            ["SelectedPlayer"] = "...";
+            ["BringingPlayer"] = false;
+            ["SpectatePlayer"] = false;
+        };
+
+        ["VehicleModifications"] = {
+            ["SpeedEnabled"] = false;
+            ["SpeedValue"] = 10/1000;
+            ["BreakEnabled"] = false;
+            ["BreakValue"] = 50/1000;
+            ["InstantStop"] = false;
+            ["InstantStopBind"] = Enum.KeyCode.V;
+        };
+
+        ["KillAura"] = false;
+        ["KillAuraRange"] = 100;
+        ["KillAuraWhitelist"] = {};
+    };
+
+    ["Game"] = {
+        ["Ray_Systems"] = (Game_Name == "Road To Riches" and {"Raycast"}
+            or Game_Name == "BlockSpin" and {"Raycast"}
+            or Game_Name == "Town" and {"Raycast"}
+            or Game_Name == "Universal" and {"Raycast", "FindPartOnRay", "FindPartOnRayWithWhitelist"}
+            or Game_Name == "The LVA" and {"FindPartOnRay"}
+            or Game_Name == "South LVA" and {"Raycast"}
+            or Game_Name == "Street Life" and {"Raycast"}
+            or Game_Name == "Criminality" and {"Raycast"}) or {};
+        ["Wall_Bang_Possible"] = (Game_Name == "Universal"
+            or Game_Name == "Criminality"
+            or Game_Name == "South LVA"
+            or Game_Name == "The LVA"
+            or Game_Name == "Street Life");
+    };
+
+    ["Aimlock"] = {
+        ["Enabled"] = false;
+        ["Aiming"] = false;
+        ["TargetPart"] = "Head";
+        ["MaxDistance"] = 300;
+        ["Mode"] = "Toggle";
+        ["Type"] = "Mouse";
+        ["Keybind"] = nil;
+        ["WallCheck"] = false;
+        ["Priority"] = {};
+        ["Whitelisted"] = {};
+        ["DrawFieldOfView"] = false;
+        ["UseFieldOfView"] = false;
+        ["Radius"] = 100;
+        ["FieldOfViewColor"] = Color3.new(1, 1, 1);
+        ["FieldOfViewTransparency"] = 0.25;
+        ["Sides"] = 100;
+        ["Smoothness"] = 1;
+        ["Snapline"] = false;
+        ["SnaplineColor"] = Color3.new(1, 1, 1);
+        ["SnaplineThickness"] = 1;
+    };
+
+    ["Silent"] = {
+        ["Enabled"] = false;
+        ["Targetting"] = false;
+        ["TargetPart"] = {"Head"};
+        ["Mode"] = "nil";
+        ["MaxDistance"] = 300;
+        ["Keybind"] = nil;
+        ["WallCheck"] = false;
+        ["WallBang"] = false;
+        ["Priority"] = {};
+        ["Whitelisted"] = {};
+        ["DrawFieldOfView"] = false;
+        ["UseFieldOfView"] = false;
+        ["Radius"] = 100;
+        ["FieldOfViewColor"] = Color3.new(1, 1, 1);
+        ["FieldOfViewTransparency"] = 0.25;
+        ["Sides"] = 100;
+        ["HitChance"] = 100;
+        ["Snapline"] = false;
+        ["SnaplineColor"] = Color3.new(1, 1, 1);
+        ["Damage"] = 100;
+        ["SnaplineThickness"] = 1;
+    };
+
+    ["WorldVisuals"] = {
+        ["SaturationEnabled"] = false;
+        ["Saturation_Value"] = 1;
+
+        ["FogColorEnabled"] = false;
+        ["FogColor"] = Color3.new(1, 1, 1);
+
+        ["AmbientEnabled"] = false;
+        ["AmbientColor"] = Color3.new(1, 1, 1);
+
+        ["FieldOfViewEnabled"] = false;
+        ["FieldOfViewValue"] = 70;
+
+        ["Fullbright"] = false;
+    };
+
+    ["MiscSettings"] = {
+        ["Hitbox_Expander"] = {
+            ["Enabled"] = false;
+            ["Multiplier"] = 15;
+            ["Color"] = Color3.new(1, 1, 1);
+            ["Transparency"] = 0;
+            ["Type"] = "Block";
+            ["Material"] = "ForceField";
+            ["Whitelist"] = {};
+            ["Part"] = "HumanoidRootPart";
+        };
+
+        ["ModifySpeed"] = {
+            ["Enabled"] = false;
+            ["Value"] = 16;
+        };
+
+        ["ModifyJump"] = {
+            ["Enabled"] = false;
+            ["Infinity"] = false;
+            ["Value"] = 50;
+        };
+
+        ["Fly"] = {
+            ["Enabled"] = false;
+            ["Type"] = "CFrame";
+            ["Speed"] = 50;
+        };
+
+        ["SpinBot"] = {
+            ["Enabled"] = false;
+            ["Speed"] = 35;
+        };
+
+        ["No-Clip"] = false;
+    };
+
+    ["Guns"] = {};
+
+    ESP = {
+        Enabled = false,
+        TeamCheck = false,
+        MaxDistance = 900,
+        FontSize = 12,
+        Font = Enum.Font.Code,
+        FadeOut = {
+            OnDistance = false,
+            OnDeath = true,
+            OnLeave = false,
+        },
+        Options = {
+            Teamcheck = true, TeamcheckRGB = Color3.fromRGB(0, 255, 0),
+            Friendcheck = true, FriendcheckRGB = Color3.fromRGB(0, 255, 0),
+            Highlight = false, HighlightRGB = Color3.fromRGB(255, 215, 0),
+        },
+        Drawing = {
+            Chams = {
+                Enabled = false,
+                Thermal = true,
+                FillRGB = Color3.fromRGB(255, 215, 0),
+                Fill_Transparency = 80,
+                OutlineRGB = Color3.fromRGB(0, 0, 0),
+                Outline_Transparency = 80,
+                VisibleCheck = false,
+            },
+            Names = {
+                Enabled = false,
+                Transparency = 0,
+                RGB = Color3.fromRGB(255, 255, 255),
+            },
+            Flags = {
+                Enabled = false,
+            },
+            Distances = {
+                Enabled = false,
+                Position = "Bottom",
+                Transparency = 0,
+                RGB = Color3.fromRGB(255, 255, 255),
+            },
+            Weapons = {
+                Enabled = false, WeaponTextRGB = Color3.fromRGB(255, 215, 0),
+                Outlined = false,
+                Gradient = false,
+                Transparency = 0,
+                GradientRGB1 = Color3.fromRGB(255, 255, 255), GradientRGB2 = Color3.fromRGB(255, 215, 0),
+            },
+            Inventory = {
+                Enabled = false, RGB = Color3.fromRGB(255, 255, 255),
+                Transparency = 0,
+            },
+            Healthbar = {
+                Enabled = false,
+                HealthText = false, Lerp = true, HealthTextRGB = Color3.fromRGB(0, 255, 0),
+                Width = 2.5,
+                Transparency = 0,
+                HealthTextTransparency = 0,
+                Gradient = true, GradientRGB1 = Color3.fromRGB(255, 215, 0), GradientRGB2 = Color3.fromRGB(0, 255, 0),
+            },
+            Boxes = {
+                Animate = true,
+                RotationSpeed = 300,
+                Gradient = false, GradientRGB1 = Color3.fromRGB(255, 215, 0), GradientRGB2 = Color3.fromRGB(0, 0, 0),
+                GradientFill = true, GradientFillRGB1 = Color3.fromRGB(255, 215, 0), GradientFillRGB2 = Color3.fromRGB(0, 0, 0),
+                Filled = {
+                    Enabled = false,
+                    Transparency = 0.75,
+                    RGB = Color3.fromRGB(0, 0, 0),
+                },
+                Full = {
+                    Enabled = true,
+                    Transparency = 0,
+                    RGB = Color3.fromRGB(255, 255, 255),
+                },
+                Bounding = {
+                    Enabled = false,
+                    Transparency = 0,
+                    RGB = Color3.fromRGB(255, 255, 255),
+                },
+                Corner = {
+                    Enabled = false,
+                    Transparency = 0,
+                    RGB = Color3.fromRGB(255, 255, 255),
+                },
+            };
+        };
+        Connections = {
+            RunService = Services.RunService;
+        };
+        Fonts = {};
+    };
+}
